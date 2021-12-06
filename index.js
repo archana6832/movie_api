c7b15e33dafa8839328a3f1633b8d3d917b81710
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-06 18:18:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -66,6 +66,17 @@
       res.status(500).send('Error: ' + err);
     });
   });
+  //Gets a user by username
+app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+  res.status(500).send('Error: ' + err);
+  });
+});
   //  <!--2. Return data about a single movie by title-->
   app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.findOne({ Title: req.params.Title })
